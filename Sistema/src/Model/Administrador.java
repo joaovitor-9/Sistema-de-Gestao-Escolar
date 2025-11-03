7410ed1c7a8997b64dea76a38c322c794dd37d9c
joaovitor-9 <joaovamorim11@gmail.com>
2025-11-02 23:14:24 -0300

    Update Administrador.java

--- Sistema/src/Model/Administrador.java
+++ Sistema/src/Model/Administrador.java
@@ -1,17 +1,12 @@
 package Model;
 
-// Esta é a ENTIDADE. Ela só guarda dados.
 public class Administrador extends Funcionario {
-
-    // (Poderia ter campos específicos, como 'nivelAcesso', 'setor', etc.)
-
+    
     public Administrador(String nome, String cpf, int idade, double salario) {
         super(nome, cpf, idade, salario);
     }
     @Override
     public String toString() {
-        // super.toString() chama o toString() de Funcionario
         return "Admin[" + super.toString() + "]";
     }
-
 }
