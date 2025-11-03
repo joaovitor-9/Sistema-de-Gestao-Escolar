de6148d0bff260789c6c15b84f562272a571e645
joaovitor-9 <joaovamorim11@gmail.com>
2025-11-03 14:28:43 -0300

    Update AdminController.java

--- Sistema/src/Controller/AdminController.java
+++ Sistema/src/Controller/AdminController.java
@@ -3,6 +3,9 @@ package Controller;
 import Model.AdminService;
 import Model.Disciplina;
 import View.SistemaView;
+import Model.Aluno;
+import Model.Nota;
+import Model.Frequencia;
 import java.util.List;
 
 public class AdminController {
@@ -10,8 +13,8 @@ public class AdminController {
     private SistemaView view;
 
     public AdminController() {
-        this.adminService = new AdminService(); 
-        this.view = new SistemaView();           
+        this.adminService = new AdminService();
+        this.view = new SistemaView();
     }
     public void iniciarCadastroAluno() {
         view.exibirMensagem("--- Cadastro de Novo Aluno ---");
@@ -20,8 +23,10 @@ public class AdminController {
             int idade = view.solicitarInt("Digite a idade: ");
             String cpf = view.solicitarString("Digite o CPF: ");
 
-            adminService.cadastrarAluno(nome, idade, cpf);
+            Aluno novoAluno = adminService.cadastrarAluno(nome, idade, cpf);
+
             view.exibirMensagem("SUCESSO: Aluno cadastrado!");
+            view.exibirMensagem("Nome: " + novoAluno.getNome() + " | Matrícula Gerada: " + novoAluno.getMatricula());
 
         } catch (Exception e) {
             view.exibirErro(e.getMessage());
@@ -36,7 +41,6 @@ public class AdminController {
             String cpf = view.solicitarString("Digite o CPF: ");
             double salario = view.solicitarDouble("Digite o salário: ");
             String especialidade = view.solicitarString("Digite a especialidade: ");
-            
             adminService.cadastrarProfessor(nome, cpf, idade, salario, especialidade);
             view.exibirMensagem("SUCESSO: Professor cadastrado!");
 
@@ -50,25 +54,20 @@ public class AdminController {
         try {
             String nome = view.solicitarString("Nome da disciplina: ");
             String codigo = view.solicitarString("Código da disciplina (ex: SI101): ");
-
             adminService.criarDisciplina(nome, codigo);
             view.exibirMensagem("SUCESSO: Disciplina criada!");
-            
         } catch (Exception e) {
             view.exibirErro(e.getMessage());
         }
         view.aguardarEnter();
     }
-
     public void iniciarMatriculaAluno() {
         view.exibirMensagem("--- Matricular Aluno em Disciplina ---");
         try {
             String matricula = view.solicitarString("Matrícula do aluno: ");
             String codigo = view.solicitarString("Código da disciplina: ");
-
             adminService.matricularAlunoEmDisciplina(matricula, codigo);
             view.exibirMensagem("SUCESSO: Aluno matriculado na disciplina!");
-            
         } catch (Exception e) {
             view.exibirErro(e.getMessage());
         }
@@ -89,10 +88,44 @@ public class AdminController {
         try {
             String idProf = view.solicitarString("ID do Professor: ");
             String codigo = view.solicitarString("Código da disciplina: ");
-
             adminService.alocarProfessorEmDisciplina(idProf, codigo);
             view.exibirMensagem("SUCESSO: Professor alocado!");
-            
+        } catch (Exception e) {
+            view.exibirErro(e.getMessage());
+        }
+        view.aguardarEnter();
+    }
+    public void iniciarConsultaAlunosPorDisciplina() {
+        view.exibirMensagem("--- Consultar Alunos por Disciplina ---");
+        try {
+            String codigo = view.solicitarString("Digite o código da disciplina: ");
+            List<Aluno> alunos = adminService.consultarAlunosDaDisciplina(codigo);
+            view.exibirMensagem("Alunos encontrados para a disciplina '" + codigo + "':");
+            view.exibirLista(alunos);
+        } catch (Exception e) {
+            view.exibirErro(e.getMessage());
+        }
+        view.aguardarEnter();
+    }
+    public void iniciarConsultaNotasAluno() {
+        view.exibirMensagem("--- Consultar Notas de um Aluno ---");
+        try {
+            String matricula = view.solicitarString("Digite a matrícula do aluno: ");
+            List<Nota> notas = adminService.consultarNotasDoAluno(matricula);
+            view.exibirMensagem("Notas encontradas para o aluno '" + matricula + "':");
+            view.exibirLista(notas);
+        } catch (Exception e) {
+            view.exibirErro(e.getMessage());
+        }
+        view.aguardarEnter();
+    }
+    public void iniciarConsultaFaltasAluno() {
+        view.exibirMensagem("--- Consultar Faltas de um Aluno ---");
+        try {
+            String matricula = view.solicitarString("Digite a matrícula do aluno: ");
+            List<Frequencia> faltas = adminService.consultarFaltasDoAluno(matricula);
+            view.exibirMensagem("Faltas encontradas para o aluno '" + matricula + "':");
+            view.exibirLista(faltas);
         } catch (Exception e) {
             view.exibirErro(e.getMessage());
         }
