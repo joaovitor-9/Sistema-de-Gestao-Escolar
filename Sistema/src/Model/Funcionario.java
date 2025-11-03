9367595e2fa1b78bea77bc862989c348d5c4716b
joaovitor-9 <joaovamorim11@gmail.com>
2025-11-03 12:57:59 -0300

    Update Funcionario.java

--- Sistema/src/Model/Funcionario.java
+++ Sistema/src/Model/Funcionario.java
@@ -3,13 +3,13 @@ package Model;
 public abstract class Funcionario extends Pessoa {
     private String id;
     private double salario;
-   public Funcionario(String nome, String cpf,int idade,double salario, String id) {
+    public Funcionario(String nome, String cpf,int idade,double salario, String id) {
         super(nome, idade,cpf);
         this.id = id;
         this.salario = salario;
     }
     public Funcionario(String nome, String cpf, int idade, double salario) {
-       super(nome, idade,cpf);
+        super(nome, idade,cpf);
         this.salario = salario;
     }
     public String getID() {
@@ -24,7 +24,7 @@ public abstract class Funcionario extends Pessoa {
     public void setSalario(double salario) {
         if (salario >= 0) {
             this.salario = salario;
-        } else {
+        }else {
             System.out.println("Salário inválido.");
         }
     }
