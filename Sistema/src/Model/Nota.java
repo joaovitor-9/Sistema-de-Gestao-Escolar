64f513e0c9f409736219aef90b30bdf558e085c1
joaovitor-9 <joaovamorim11@gmail.com>
2025-11-02 23:18:54 -0300

    Update Nota.java

--- Sistema/src/Model/Nota.java
+++ Sistema/src/Model/Nota.java
@@ -1,7 +1,6 @@
 package Model;
 
 public class Nota {
-
     private final Aluno aluno;
     private final Disciplina disciplina;
     private double valorNota;
@@ -11,26 +10,21 @@ public class Nota {
         this.disciplina = disciplina;
         setValorNota(valorNota);
     }
-
     public Aluno getAluno() {
         return aluno;
     }
-
     public Disciplina getDisciplina() {
         return disciplina;
     }
-
     public double getValorNota() {
         return valorNota;
     }
-
     public void setValorNota(double valorNota) {
         if (valorNota < 0 || valorNota > 10) {
             throw new IllegalArgumentException("A nota deve estar entre 0 e 10.");
         }
         this.valorNota = valorNota;
     }
-
     @Override
     public String toString() {
         return "Nota[Disciplina: " + disciplina.getNome() + ", Aluno: " + aluno.getNome() + ", Valor: " + valorNota + "]";
